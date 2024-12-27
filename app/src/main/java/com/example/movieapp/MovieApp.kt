fffc173fd3130708a72e04a79609d05ea82bf58c
alRyumin <alryumin@gmail.com>
2024-12-27 12:20:39 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/movieapp/MovieApp.kt
@@ -0,0 +1,20 @@
+package com.example.movieapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.movieapp.ui.screen.HomeScreen
+
+@Composable
+fun MovieApp() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Surface(
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            HomeScreen()
+        }
+    }
+}
